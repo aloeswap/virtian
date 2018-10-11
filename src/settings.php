94809cfd7280693ed6eca7175597c8b8d272d24d
Softsun <39539616+softsun-cz@users.noreply.github.com>
2018-10-11 02:08:51 +0200

    Add files via upload

---
+++ src/settings.php
@@ -0,0 +1,11 @@
+<?php
+ $GLOBALS['product']       = 'Virtian';
+ $GLOBALS['version']       = '0.01';
+ $GLOBALS['company']       = 'Softsun s.r.o.';
+ $GLOBALS['web']           = 'https://github.com/softsun-cz/vm-webui';
+ $GLOBALS['year-from']     = '2018';
+ $GLOBALS['template-path'] = 'templates/default';
+ $GLOBALS['iso-path']      = '/iso';
+ $GLOBALS['password']      = 'admin123';
+ $GLOBALS['address']       = 'qemu:///system';
+?>
