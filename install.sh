48aebab126733cf6fcca9e105e6835df2e8aa6e9
Softsun <39539616+softsun-cz@users.noreply.github.com>
2018-10-12 16:12:19 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 apt-get update
 apt-get -y upgrade
-apt-get -y install mc ssh net-tools nginx php-fpm php-imagick php-gd vnstat xfsprogs php-libvirt-php
+apt-get -y install mc ssh net-tools nginx php-fpm php-imagick php-gd vnstat xfsprogs libvirt-clients libvirt-daemon libvirt-daemon-system php-libvirt-php
 alias ls="ls --color=always"
 rm -rf /var/www/html
 openssl req -x509 -newkey rsa:2048 -nodes -days $(expr '(' $(date -d 2999/01/01 +%s) - $(date +%s) + 86399 ')' / 86400) -subj "/" -keyout /etc/nginx/nginx.key -out /etc/nginx/nginx.crt
