dc16fa38cd797e034fa53a50984dd9ea21cf89ac
Softsun <39539616+softsun-cz@users.noreply.github.com>
2018-10-17 11:32:50 +0200

    Update settings.php

--- src/settings.php
+++ src/settings.php
@@ -2,7 +2,7 @@
  $GLOBALS['product']       = 'Virtian';
  $GLOBALS['version']       = '0.01';
  $GLOBALS['company']       = 'Softsun s.r.o.';
- $GLOBALS['web']           = 'https://github.com/softsun-cz/vm-webui';
+ $GLOBALS['web']           = 'https://virtian.org';
  $GLOBALS['year-from']     = '2018';
  $GLOBALS['template-path'] = 'templates/default';
  $GLOBALS['iso-path']      = '/iso';
